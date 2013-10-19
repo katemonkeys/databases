0f7cac70e5e9c8af32e997aefd1e3dc3744dc376
Kate Jenkins <catherine.ann.jenkins@gmail.com>
2013-10-19 12:01:07 -0700

    Handles GET and explores mysql server problem space. Moves all server calls to persistent_server

--- SQL/persistent_server.js
+++ SQL/persistent_server.js
@@ -1,28 +1,108 @@
+var http = require("http");
 var mysql = require('mysql');
-var basic = require(__dirname + '/server/basic-server');
+var url = require('url');
+var fs = require('fs');
+// var requestHandler = require("./request-handler.js");
 // var handler = require(__dirname + '/server/request-handler');
+
+var port = 8080;
+var ip = "127.0.0.1";
+var _chatrooms = {lobby: [{username: "bill", message: "this is dummby data", roomname: "lobby", createdAt: new Date()}]};//{lobby: 'lobby'};
 
 var dbConnection = mysql.createConnection({
   user: "root",
   password: "",
-  database: "chat"
+  database: "chat",
+  port: 3306,
+  host: ip
 });
-
 dbConnection.connect();
-/* Now you can make queries to the Mysql database using the
- * dbConnection.query() method.
- * See https://github.com/felixge/node-mysql for more details about
- * using this module.*/
 
-/* You already know how to create an http server from the previous
- * assignment; you can re-use most of that code here. */
+// dbConnection.query("describe messages;", function(err,data){
+//   if (err) throw err;
+//   console.log(data);
+// });
 
-module.exports.getFromDB = function(room_ID){
-  room_ID = room_ID || 1;
-  var queryString = 'Select * from messages where room_ID =' + room_ID +";";
-  return dbConnection.query(queryString, function(err){
-    if (err) throw err;
-    console.log("retrieved from database");
+var requestListener = function (request, response) {
+  console.log("Serving request type " + request.method + " for url " + request.url);
+  var headers =  {
+    "access-control-allow-origin": "*",
+    "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
+    "access-control-allow-headers": "content-type, accept",
+    "access-control-max-age": 10, // Seconds.
+    "content-type": "application/json"
+  };
+
+//FIXME: 
+  var chatroom = (url.parse(request.url).pathname).split('/')[2];  //TODO: this is a hack!! fix me!
+  // Switches between request methods
+  if (chatroom) {
+    _chatrooms[chatroom] = ( _chatrooms[chatroom] ) ? _chatrooms[chatroom]: [];
+    if (request.method === 'GET'){
+      handleGet(request, response, headers, chatroom);
+    } else if (request.method === 'POST'){
+      handlePost(request, response, headers, chatroom);
+    } else if (request.method === 'OPTIONS'){
+      response.writeHead(200, headers);
+      response.end();
+    }
+  } else {
+    response.writeHead(404, headers);
+    response.end();
+  }
+};
+
+
+//
+//  ---------- GET Request Method Handlers ---------------------
+//
+  //room_ID should be passed in getFromDB however
+  // currently chatroom is a string not connected to ID
+  // to fix in refactor
+var handleGet = function(request, response, headers, chatroom) {
+  response.writeHead(200, headers);
+  return getFromDB(1, function(data){
+    console.log("GET response from Database: ", data);
+    response.end(JSON.stringify( data ));
   });
 };
 
+var getFromDB = function(room_ID, cb){
+  room_ID = room_ID || 1;
+  var queryString = 'select * from messages where room_ID = '+room_ID+';';
+  return dbConnection.query(queryString, function(err, data){
+    if (err) throw err;
+    cb(data);
+  });
+};
+
+
+var getRoomID = function(chatroom, cb){
+  var queryString = 'SELECT * FROM rooms WHERE name=\''+chatroom+'\';';
+  return dbConnection.query(queryString, function(err,data){
+    if (err) throw err;
+    cb(data[0].id);
+  });
+};
+//this is a test
+//
+//  ---------- POST Request Method Handlers ---------------------
+//
+var handlePost = function(request, response, headers, chatroom, userID) {
+  var body = "";
+  request.on('data', function(data){
+    body += data;
+  });
+  request.on('end', function(){
+    var POST = JSON.parse(body);
+    POST.createdAt = new Date();
+    _chatrooms[chatroom].push(POST);
+  });
+  response.writeHead(201, headers);
+  response.end();
+};
+
+
+// ---------- this starts the server after everything is initalized
+var server = http.createServer(requestListener);
+server.listen(port, ip);
